c3ef688a9d853507f9091007fbbb3e71e487dbf3
Raviprasad <78257726+raviprasadchowdhary@users.noreply.github.com>
2025-12-31 21:50:50 +0530

    constants

---
+++ JSFundamentals/lessons/constants.js
@@ -0,0 +1,12 @@
+const OCCUPATION = "Engineer"
+console.log("Occupation: " + OCCUPATION)
+// OCCUPATION = "Doctor" // This will cause an error because OCCUPATION is a constant
+console.log("New Occupation: " + OCCUPATION)
+
+
+// constants can't be created without a value
+// const age; // This will cause an error because a constant must be initialized unlike a variable
+const BIRTH_YEAR = 1990
+console.log("Birth Year: " + BIRTH_YEAR)
+// BIRTH_YEAR = 2000 // This will cause an error because BIRTH_YEAR is a constant
+console.log("New Birth Year: " + BIRTH_YEAR)
