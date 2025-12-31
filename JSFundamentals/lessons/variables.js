83ef628e3a0d9e396b0ca3bce1fd0b5f9b55b6ea
Raviprasad <78257726+raviprasadchowdhary@users.noreply.github.com>
2025-12-31 20:56:31 +0530

    js fundamentals variables

---
+++ JSFundamentals/lessons/variables.js
@@ -0,0 +1,15 @@
+// variables
+var firstName = "John"
+let lastName = "Smith"
+
+console.log("Full name: " + firstName + " " + lastName)
+
+var age, dateOfBirth, sex
+age = 25
+sex = "Male"
+
+console.log("Age: " + age)
+console.log("Sex: " + sex)
+
+age = 26
+console.log("New Age: " + age)
