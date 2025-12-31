93fbb01ebc8cd4a42e708df701a94dd825cd6428
Raviprasad <78257726+raviprasadchowdhary@users.noreply.github.com>
2025-12-31 23:43:24 +0530

    object with array

--- JSFundamentals/Lesson3/objects.js
+++ JSFundamentals/Lesson3/objects.js
@@ -16,3 +16,14 @@ person['lastName'] = "Sharma"
 console.log(`Updated Full name: ${person.firstName} ${person.lastName}`)
 
 console.log(`accssing first element using bracket notation: ${person['firstName']}`)
+
+// object with array
+var employee = {
+    firstname: "Amar",
+    lastName: "Kumar",
+    skills: ['javaScript', 'python', 'c++']
+}
+
+console.log(`Employee object: ${JSON.stringify(employee)}`)
+console.log(`Employee Skills: ${employee.skills}`)
+console.log(`First Skill: ${employee.skills[0]}`)
