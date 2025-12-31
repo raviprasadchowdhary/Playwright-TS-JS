bf8f8ee7fe6f7055342cfce3aed1d2ac02a9e78c
Raviprasad <78257726+raviprasadchowdhary@users.noreply.github.com>
2025-12-31 22:49:14 +0530

    concatination

---
+++ JSFundamentals/Lesson2/concatination.js
@@ -0,0 +1,9 @@
+var item = "Diet Coke"
+var price = 40
+message = "The price for your " + item + " is " + price + " INR."
+console.log(message)
+
+item = "Pepsi"
+price = 35
+message = "The price for your " + item + " is " + price + " INR."
+console.log(message)
