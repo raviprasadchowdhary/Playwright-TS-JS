df636d37374c92e637d3a2dcdfdbd9664ffcbbd4
Raviprasad <78257726+raviprasadchowdhary@users.noreply.github.com>
2025-12-31 22:49:26 +0530

    interpolation

---
+++ JSFundamentals/Lesson2/interpolation.js
@@ -0,0 +1,9 @@
+var employee = 'Raviprasad'
+var workLocation = "Bangalore"
+var message = `${employee}'s work location is ${workLocation}`
+console.log(message)
+
+employee = "Rohan"
+workLocation = "Pune"
+message = `${employee}'s work location is ${workLocation}`
+console.log(message)
