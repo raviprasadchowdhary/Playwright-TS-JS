0e23d8b2990879c178528f901a01910b48180643
Raviprasad <78257726+raviprasadchowdhary@users.noreply.github.com>
2025-12-31 22:16:23 +0530

    data types

---
+++ JSFundamentals/lessons/dataTypes.js
@@ -0,0 +1,9 @@
+var firstname = 'Brock'
+var lastName = "McMorran"
+var middleInitial = `A`
+var age = 25
+var isMarried = false
+var marriageYear = null
+var address = undefined
+
+console.log("Full name: " + firstname + " " + middleInitial + " " + lastName)
