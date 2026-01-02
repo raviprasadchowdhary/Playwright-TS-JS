cb0f2465a6cae8833af9bda0ccd0d66210d85fb3
Raviprasad <78257726+raviprasadchowdhary@users.noreply.github.com>
2026-01-02 23:27:27 +0530

    datatyoes

--- JSFundamentals/Lesson1/dataTypes.js
+++ JSFundamentals/Lesson1/dataTypes.js
@@ -1,9 +1,9 @@
-var firstname = 'Brock'
-var lastName = "McMorran"
-var middleInitial = `A`
-var age = 25
-var isMarried = false
-var marriageYear = null
-var address = undefined
+var firstname = 'Brock' //data type: string; you can use single quotes
+var lastName = "McMorran" //data type: string; you can use double quotes
+var middleInitial = `A` //data type: string; you can use backticks
+var age = 25 //data type: number
+var isMarried = false //data type: boolean
+var marriageYear = null //data type: null
+var address = undefined //data type: undefined
 
 console.log("Full name: " + firstname + " " + middleInitial + " " + lastName)
