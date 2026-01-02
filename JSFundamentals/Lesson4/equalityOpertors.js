7e2b967febfc52ed738f3baccff01f095048f63e
Raviprasad <78257726+raviprasadchowdhary@users.noreply.github.com>
2026-01-02 22:49:18 +0530

    equality operators

---
+++ JSFundamentals/Lesson4/equalityOpertors.js
@@ -0,0 +1,14 @@
+// Equality Operators
+// Equal to ==
+console.log(`is (1==1)? ` + ((1==1)))
+console.log(`is (1=='1')? ` + ((1=='1')))
+console.log(`is (1==2)? ` + ((1==2)))
+// Not Equal to !=
+console.log(`is (1!=1)? ` + ((1!=1)))
+console.log(`is (1!=2)? ` + ((1!=2)))
+// Strict Equal to ===
+console.log(`is (1===1)? ` + ((1===1)))
+console.log(`is (1==='1')? ` + ((1==='1')))
+// Strict Not Equal to !==
+console.log(`is (1!=='1')? ` + ((1!=='1')))
+console.log(`is (1!==1)? ` + ((1!==1)))
