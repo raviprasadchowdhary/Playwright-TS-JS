0703b7c5486e9d279e4750451f9cb3cf9e17ad67
Raviprasad <78257726+raviprasadchowdhary@users.noreply.github.com>
2026-01-02 23:27:44 +0530

    relational operators

--- JSFundamentals/Lesson4/relationaloperators.js
+++ JSFundamentals/Lesson4/relationaloperators.js
@@ -1,5 +1,3 @@
-// Relational or Comparison Operators
-
 // Examples of relational operators
 // Greater than >
 console.log(`is (5>3)? ` + (5>3))
@@ -13,27 +11,3 @@ console.log(`is (6<5)? ` + (6<5))
 // Less than or Equal to <=
 console.log(`is (5<=5)? ` + (5<=5))
 console.log(`is (6<=5)? ` + (6<=5))
-
-// Equality Operators
-// Equal to ==
-console.log(`is (1==1)? ` + ((1==1)))
-console.log(`is (1=='1')? ` + ((1=='1')))
-console.log(`is (1==2)? ` + ((1==2)))
-// Not Equal to !=
-console.log(`is (1!=1)? ` + ((1!=1)))
-console.log(`is (1!=2)? ` + ((1!=2)))
-// Strict Equal to ===
-console.log(`is (1===1)? ` + ((1===1)))
-console.log(`is (1==='1')? ` + ((1==='1')))
-// Strict Not Equal to !==
-console.log(`is (1!=='1')? ` + ((1!=='1')))
-console.log(`is (1!==1)? ` + ((1!==1)))
-
-// Ternary Operator ? :
-var agePerson1 = 20
-var canVote = (agePerson1>=18)?'Yes':'No'
-console.log(`A person with age:${agePerson1}canVote?: ${canVote}`)
-
-var agePerson2 = 17
-var canVote = (agePerson2>=18)?'Yes':'No'
-console.log(`A person with age:${agePerson2}canVote?: ${canVote}`)
