05bb9b4ca39d1fb3bc633e22b5a4b5bdd566b160
Raviprasad <78257726+raviprasadchowdhary@users.noreply.github.com>
2026-01-02 22:49:38 +0530

    ternary operators

---
+++ JSFundamentals/Lesson4/ternaryOperators.js
@@ -0,0 +1,8 @@
+// Ternary Operator ? :
+var agePerson1 = 20
+var canVote = (agePerson1>=18)?'Yes':'No'
+console.log(`A person with age:${agePerson1}canVote?: ${canVote}`)
+
+var agePerson2 = 17
+var canVote = (agePerson2>=18)?'Yes':'No'
+console.log(`A person with age:${agePerson2}canVote?: ${canVote}`)
