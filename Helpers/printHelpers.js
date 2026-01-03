afbf3f20c31397bd5839f78e14eecb7bfca9a118
Raviprasad <78257726+raviprasadchowdhary@users.noreply.github.com>
2026-01-03 23:24:33 +0530

    class and method

--- Helpers/printHelpers.js
+++ Helpers/printHelpers.js
@@ -1,3 +1,29 @@
 export function printAge(age){
     console.log(`Age is: ${age}`)
 }
+
+class EmployeeDetails{
+
+    // function to print employee details
+
+    // method to print employee age
+    /**
+     * This method prints the age of the employee
+     * @param {integer} age 
+     */ 
+    printAge(age){
+        console.log(`Employee age is: ${age}`)
+    }
+
+    // method to print employee name
+    /**
+     * This method prints the name of the employee
+     * @param {String} firstName 
+     * @param {String} lastName 
+     */
+    printName(firstName, lastName){
+        console.log(`Employee name is: ${firstName} ${lastName}`)
+    }
+}
+
+export const employeeDetails = new EmployeeDetails()
---
+++ JSFundamentals/Lesson8/classAndMethods.js
@@ -0,0 +1,6 @@
+// classes and methods
+import { employeeDetails } from '../../Helpers/printHelpers.js'
+
+// using methods from EmployeeDetails class
+employeeDetails.printAge(25)
+employeeDetails.printName('Raviprasad', 'Chowdhary')
