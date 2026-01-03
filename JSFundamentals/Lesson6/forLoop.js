21e4afaee258383139e81c36b9f7562dcafcc058
Raviprasad <78257726+raviprasadchowdhary@users.noreply.github.com>
2026-01-03 13:21:00 +0530

    for loop

--- JSFundamentals/Lesson6/forLoop.js
+++ JSFundamentals/Lesson6/forLoop.js
@@ -1,5 +1,3 @@
-// loops
-
 // for loop
 /*
     for (statement1; statement2; statement3) {
@@ -24,13 +22,3 @@ console.log(`Example 3: Print numbers from 5 to 1`)
 for (let i=5; i>=1; i--){
     console.log(i)
 }
-
-// while loop
-/*
-    while (condition) {
-    // code block to be executed
-    }
-    // The code block inside the loop will be executed as long as the condition is true.
-    // If the condition is false at the beginning, the code block will not be executed at all.
-    // Make sure to update the condition inside the loop to avoid infinite loops. 
-*/
